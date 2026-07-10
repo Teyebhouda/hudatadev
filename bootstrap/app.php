018cc2ee87126776d9aab49b7011356571296a8f
teyebhouda <teyebhouda1994@gmail.com>
2026-07-11 00:53:42 +0200

    lodflare

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+         $middleware->alias([
+        'anti-spam' => \App\Http\Middleware\AntiSpamMiddleware::class,
+    ]);
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
